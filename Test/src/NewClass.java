9f047c012a7dea1e0ed2ecc640c2053158899030
Bruno <fbra@sunyit.edu>
2013-07-16 13:51:10 -0700

    New branch for NewClass.java

--- Test/src/NewClass.java
+++ Test/src/NewClass.java
@@ -4,8 +4,10 @@ public class NewClass {
 	public void f() {
 		int x = 0;
 		double y = 34.56;
+		boolean  b = true;
 		System.out.println(x);
 		System.out.println(y);
+		System.out.println(b);
 	}
    
 }
